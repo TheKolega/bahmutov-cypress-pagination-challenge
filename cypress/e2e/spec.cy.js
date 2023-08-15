099527267bdbbdc54e87408028cf82c9f89ea850
Miloš Jovanović <milos.jov.rs@gmail.com>
2023-08-15 23:59:39 +0200

    Add cypress-if solution to spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,15 +5,16 @@
 beforeEach(() => {
   cy.visit('public/index.html')
 })
-
-it('clicks the Next button until we get to the last page', () => {
-  // the HTML table on the page is paginated
-  // can you click the "Next" button until
-  // we get to the very last page?
-  // button selector "[value=next]"
-  cy.get('[value=next]') // keep clicking!
-
-  cy.log('**confirm we are on the last page**')
-  cy.get('[value=next]').should('be.disabled')
-  cy.get('[value=last]').should('be.disabled')
+describe('With cypress-if', () => {
+  it('clicks the Next button until we get to the last page', () => {
+    // the HTML table on the page is paginated
+    // can you click the "Next" button until
+    // we get to the very last page?
+    // button selector "[value=next]"
+    cy.keepClicking('[value=next]').then(() => {
+      cy.log('**confirm we are on the last page**')
+      cy.get('[value=next]').should('be.disabled')
+      cy.get('[value=last]').should('be.disabled')
+    })
+  })
 })
